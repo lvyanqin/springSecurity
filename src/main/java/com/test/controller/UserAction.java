4ecaae9174ace35cedfd1e651389d9cfa8f314d6
lvyanqin <lvyanqin@114study.com>
2017-04-24 17:22:16 +0800

    jdbc

--- src/main/java/com/test/controller/UserAction.java
+++ src/main/java/com/test/controller/UserAction.java
@@ -5,7 +5,12 @@
  */
 package com.test.controller;
 
+import com.test.beans.UserBean;
+import com.test.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -14,6 +19,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserAction {
     
+    @Autowired
+    private UserMapper userMapper;
     
+    @RequestMapping(value = "/user")
+    @ResponseBody
+    public String user(){
+        UserBean user = userMapper.findById(48);
+        return "1";
+    }
     
 }
