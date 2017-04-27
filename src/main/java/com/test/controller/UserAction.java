a4f013114f5b0dcf29d7dbb0da9969de4b303445
lvyanqin <lvyanqin@114study.com>
2017-04-27 17:42:59 +0800

    restful post

--- src/main/java/com/test/controller/UserAction.java
+++ src/main/java/com/test/controller/UserAction.java
@@ -9,8 +9,11 @@ import com.test.beans.UserBean;
 import com.test.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -28,6 +31,27 @@ public class UserAction {
 
         UserBean user = userMapper.findById(48);
         return "1";
+    }
+    
+    @RequestMapping(value = "/user/init")
+    public ModelAndView userHtml(Integer id){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/user");
+        return mv;
+    }
+    
+    @RequestMapping(value = "/user/test", method = RequestMethod.POST)
+    @ResponseBody
+    public String testRestfulPost(Integer id){
+        UserBean user = userMapper.findById(id);
+        return "2";
+    }
+    
+    @RequestMapping(value = "/user/test2/{flag}", method = RequestMethod.POST)
+    @ResponseBody
+    public String testRestfulPost2(@PathVariable String flag, Integer id){
+        UserBean user = userMapper.findById(id);
+        return flag;
     }
     
 }
--- src/main/java/com/test/mapper/UserMapper.java
+++ src/main/java/com/test/mapper/UserMapper.java
@@ -6,6 +6,7 @@
 package com.test.mapper;
 
 import com.test.beans.UserBean;
+import java.util.List;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
